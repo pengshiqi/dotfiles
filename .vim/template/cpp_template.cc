544672d3ec2bbdd4ad500beef28dd6cc6f7fba9a
pengshiqi <617225573@qq.com>
2019-08-08 10:34:33 +0800

    Update: vim

---
+++ .vim/template/cpp_template.cc
@@ -0,0 +1,40 @@
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <fstream>
+#include <list>
+#include <map>
+#include <iostream>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <vector>
+
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define forn(i, n) for (int i = 0; i < (int)(n); ++i)
+#define for1(i, n) for (int i = 1; i <= (int)(n); ++i)
+#define ford(i, n) for (int i = (int)(n) - 1; i >= 0; --i)
+#define fore(i, a, b) for (int i = (int)(a); i <= (int)(b); ++i)
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+
+const int MOD = 1000000007;
+const ll INF = 1e18;
+
+int main() {
+    
+    return 0;
+}
